cd8e007328cf35d3a2b040e3c108d415a1e531e7
SancakOnline <osmanaksoy1661@gmail.com>
2025-12-20 19:19:07 +0300

    kamera

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;   // Karakter
+    public float smoothSpeed = 5f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(
+            transform.position,
+            desiredPosition,
+            smoothSpeed * Time.deltaTime
+        );
+
+        transform.position = smoothedPosition;
+    }
+}
