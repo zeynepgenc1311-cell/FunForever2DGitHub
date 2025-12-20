0f8ca643243dd2ca553ed16b7ad6b09721f55b06
yukiouuu <zeynep.genc1311@gmail.com>
2025-12-20 05:03:32 +0300

    shopmanager

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject sellContent;
+    [SerializeField] private List<Item> sellItems = new();
+    [SerializeField] private ShopSlot ShopSlotPrefab;
+    
+
+
+    private void Start()
     {
-        
+        SellFillSlots(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    public ShopState currentState = ShopState.Sell;
+
+    public void MarketRequest(Item item, int amount)
     {
-        
+        switch (currentState)
+        {
+            case ShopState.Sell:
+            bool condition = Inventory.Instance.RemoveItem(item, amount);
+            if (condition)
+            {
+                CurrencyManager.Instance.AddCoins(item.itemPrice);
+            }
+            else Debug.Log("item sende yok satılmadı");
+            break;
+        }
     }
+
+
+    public void SellFillSlots()
+    {
+        foreach (Item item in sellItems)
+        {
+            ShopSlot slot = Instantiate(ShopSlotPrefab, sellContent.transform.position, Quaternion.identity);
+            slot.transform.SetParent(sellContent.transform);
+            slot.Initialize(item);
+        }
+    }
+    
+    public static ShopManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
+
+[System.Serializable]
+public enum ShopState
+{
+    Sell
 }
--- Assets/Scripts/ShopSlot.cs
+++ Assets/Scripts/ShopSlot.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class ShopSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   [SerializeField] private Image slotIcon;
+   [SerializeField] private Text priceText;
+   [SerializeField] private Button slotButton;
+   public Item currentItem;
+   private void Start()
     {
-        
+        slotButton.onClick.AddListener(() =>
+        {
+            ShopManager.Instance.MarketRequest(currentItem, 1);
+        });
+        Initialize(currentItem);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Initialize(Item newItem)
     {
-        
+        currentItem = newItem;
+        if(currentItem != null)
+        {
+            slotIcon.enabled = true;
+            slotIcon.sprite = currentItem.itemSprite;
+            priceText.text = currentItem.itemPrice.ToString();
+        }
+        else
+        {
+            slotIcon.enabled = false;
+            priceText.text = "";
+        }
     }
 }
